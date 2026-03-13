36d011198bf9e9b6278afb778d2d5e9ff44c3d8a
Macohi <sinconsistencia@gmail.com>
2026-03-12 20:41:09 -0500

    fix: disable flixel cursor

--- source/Main.hx
+++ source/Main.hx
@@ -1,3 +1,4 @@
+import flixel.FlxG;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
@@ -6,6 +7,9 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+
 		addChild(new FlxGame(0, 0, StateGame));
+
+		FlxG.mouse.useSystemCursor = true;
 	}
 }
