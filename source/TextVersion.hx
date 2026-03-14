3337f1b0ffb5f45de91f110e561c311e7e6cc2ed
Macohi <sinconsistencia@gmail.com>
2026-03-14 00:04:27 -0500

    chore: version format changes

--- source/TextVersion.hx
+++ source/TextVersion.hx
@@ -4,6 +4,6 @@ class TextVersion extends Text
 {
 	override public function new(?x:Float, ?y:Float)
 	{
-		super('Survivecave Indev ${Application.current.meta.get('version')}', x, y);
+		super('${Application.current.meta.get('version')}', x, y);
 	}
 }
