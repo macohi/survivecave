69e7bf15f10443859e7eaa1b679b613f0a563e74
Macohi <sinconsistencia@gmail.com>
2026-03-14 20:06:07 -0500

    revert: shop is now scrapped

--- source/objects/sprites/SpriteShop.hx
+++
@@ -1,9 +0,0 @@
-package objects.sprites;
-
-class SpriteShop extends Sprite
-{
-	override public function new(?x:Float, ?y:Float)
-	{
-		super('shop', x, y);
-	}
-}
--- source/objects/sprites/SpriteShopGUI.hx
+++
@@ -1,9 +0,0 @@
-package objects.sprites;
-
-class SpriteShopGUI extends SpriteGUI
-{
-	override public function new(?x:Float, ?y:Float)
-	{
-		super('shop', x, y);
-	}
-}
